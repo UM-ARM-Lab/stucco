bdf2384cd1184e04a8b6d805f323d5e20a36a962
Sheng Zhong <zhsh@umich.edu>
2022-07-21 16:10:24 -0400

    Update setup to include more test packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stucco',
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(),
     url='',
     license='',
@@ -11,6 +11,9 @@ setup(
     description='',
     test_suite='pytest',
     tests_require=[
-        'pytest', 'pybullet', 'pynput', 'trimesh'
-    ], install_requires=['matplotlib', 'numpy', 'scipy', 'scikit-learn', 'torch']
+        'pytest', 'pybullet', 'pynput', 'trimesh', 'pymeshlab', 'mesh-to-sdf'
+        'window_recorder @ git+ssh://git@github.com/LemonPi/window_recorder.git'
+    ], install_requires=[
+        'matplotlib', 'numpy', 'scipy', 'scikit-learn', 'torch', 'gpytorch',
+    ]
 )
