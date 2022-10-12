82bceb0e690f90df21e1bf71344dcb189cfb8a14
Sheng Zhong <zhsh@umich.edu>
2022-10-12 15:47:19 -0400

    Create generic poking controller

--- scripts/registration_experiments.py
+++ scripts/registration_experiments.py
@@ -1,4 +1,6 @@
 import abc
+import enum
+import itertools
 import typing
 import re
 import copy
@@ -48,6 +50,8 @@ from stucco.icp import costs as icp_costs
 from stucco import util
 from stucco.sdf import ObjectFactory
 
+from arm_pytorch_utilities.controller import Controller
+from stucco import tracking, detection
 from stucco.retrieval_controller import sample_mesh_points, TrackingMethod, OurSoftTrackingMethod, \
     SklearnTrackingMethod, PHDFilterTrackingMethod
 
@@ -1014,10 +1018,103 @@ def draw_pose_distribution(link_to_world_tf_matrix, obj_id_map, dd, obj_factory:
         if sequential_delay is not None:
             b = 0
             time.sleep(sequential_delay)
-        
+
         object_id = obj_id_map.get(b, None)
         object_id = obj_factory.draw_mesh(dd, "icp_distribution", (pos, rot), (0, 0.8, 0.2, 0.2), object_id=object_id)
         obj_id_map[b] = object_id
+
+
+class PokingController(Controller):
+    class Mode(enum.Enum):
+        GO_TO_NEXT_TARGET = 0
+        PUSH_FORWARD = 1
+        RETURN_BACKWARD = 2
+        DONE = 3
+
+    def __init__(self, contact_detector: detection.ContactDetector, contact_set: tracking.ContactSet,
+                 y_order=(0, 0.2, 0.3, -0.2, -0.3), z_order=(0.05, 0.15, 0.25, 0.325, 0.4, 0.5), x_rest=-0.05,
+                 Kp=30,
+                 push_forward_count=10, nu=3, dim=3, goal_tolerance=3e-4):
+        super().__init__()
+
+        self.x_rest = x_rest
+        self.target_yz = list(itertools.product(y_order, z_order))
+        self.push_forward_count = push_forward_count
+        self.mode = self.Mode.GO_TO_NEXT_TARGET
+        self.kp = Kp
+
+        self.goal_tolerance = goal_tolerance
+
+        # primitive state machine where we push forward for push_forward_count, then go back to x_rest
+        self.push_i = 0
+        self.i = 0
+        self.current_target = None
+
+        self.contact_detector = contact_detector
+        self.contact_set = contact_set
+        self.dim = dim
+        self.contact_indices = []
+
+        self.nu = nu
+
+        self.x_history = []
+        self.u_history = []
+
+    def update(self, obs, info, visualizer=None):
+        if self.contact_detector.in_contact():
+            self.contact_indices.append(self.i)
+
+        x = self.x_history[-1][:self.dim]
+        pt, dx = self.contact_detector.get_last_contact_location(visualizer=visualizer)
+
+        info['u'] = torch.tensor(self.u_history[-1])
+        self.contact_set.update(x, dx, pt, info=info)
+
+    def done(self):
+        return len(self.target_yz) == 0
+
+    def command(self, obs, info=None, visualizer=None):
+        self.x_history.append(obs)
+
+        if len(self.x_history) > 1:
+            self.update(obs, info, visualizer=visualizer)
+
+        u = [0 for _ in range(self.nu)]
+        if self.done():
+            self.mode = self.Mode.DONE
+        else:
+            if self.mode == self.Mode.GO_TO_NEXT_TARGET:
+                # go to next target proportionally
+                target = self.target_yz[0]
+                diff = np.array(target) - np.array(obs[1:])
+                # TODO clamp?
+                u[1] = diff[0] * self.kp
+                u[2] = diff[1] * self.kp
+
+                if np.linalg.norm(diff) < self.goal_tolerance:
+                    self.mode = self.Mode.PUSH_FORWARD
+                    self.push_i = 0
+
+            # if we don't have a current target, find the next
+            elif self.mode == self.Mode.PUSH_FORWARD:
+                u[0] = 1.
+                self.push_i += 1
+                if self.push_i >= self.push_forward_count:
+                    self.mode = self.Mode.RETURN_BACKWARD
+            elif self.mode == self.Mode.RETURN_BACKWARD:
+                diff = self.x_rest - obs[0]
+                u[0] = diff * self.kp
+
+                if abs(diff) < self.goal_tolerance:
+                    self.mode = self.Mode.GO_TO_NEXT_TARGET
+                    self.target_yz = self.target_yz[1:]
+                    u = None
+
+            self.i += 1
+
+        if u is not None:
+            self.u_history.append(u)
+        return u
 
 
 def run_poke(env: poke.PokeEnv, method: TrackingMethod, reg_method, name="", seed=0, clean_cache=False,
@@ -1032,7 +1129,8 @@ def run_poke(env: poke.PokeEnv, method: TrackingMethod, reg_method, name="", see
     else:
         cache = pd.DataFrame()
 
-    ctrl = method.create_controller(predetermined_controls()[env.level])
+    # ctrl = method.create_controller(predetermined_controls()[env.level])
+    ctrl = PokingController(env.contact_detector, method.contact_set)
 
     obs = env.reset()
 
@@ -1074,6 +1172,9 @@ def run_poke(env: poke.PokeEnv, method: TrackingMethod, reg_method, name="", see
                                                vis=env.vis, debug=False)
 
     rand.seed(seed)
+    # create the action noise before sampling to ensure they are the same across methods
+    action_noise = np.random.randn(5000, 3) * ctrl_noise_max
+
     while not ctrl.done():
         best_distance = None
         simTime += 1
@@ -1183,7 +1284,7 @@ def run_poke(env: poke.PokeEnv, method: TrackingMethod, reg_method, name="", see
                 action = action.cpu()
 
             action = np.array(action).flatten()
-            action += np.random.randn(action.shape[0]) * ctrl_noise_max
+            action += action_noise[simTime]
             obs, rew, done, info = env.step(action)
 
     if reg_method == icp.ICPMethod.NONE:
@@ -1534,6 +1635,7 @@ if __name__ == "__main__":
         def filter(cache):
 
             return cache
+
 
         plot_icp_results(filter=filter, icp_res_file=f"poking_{obj_factory.name}.pkl",
                          key_columns=("method", "name", "seed", "poke", "batch"), logy=True,
