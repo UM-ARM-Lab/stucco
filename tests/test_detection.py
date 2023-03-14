18cbdb191a87ae022d10126b9b6e8b2273876c28
Sheng Zhong <zhsh@umich.edu>
2023-03-13 22:42:16 -0400

    Visualize using open3d to accelerate point plotting instead of pybullet

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -1,10 +1,11 @@
+import copy
 import os
 import torch
 import numpy as np
 import pytorch_kinematics as pk
 import pytorch_volumetric as pv
-import pybullet as p
 import matplotlib.pyplot as plt
+import open3d as o3d
 import logging
 from stucco import detection
 
@@ -19,6 +20,27 @@ TEST_DIR = os.path.dirname(__file__)
 
 
 def test_3d_contact_detection():
+    visualize = True
+    first_rotate = False
+
+    def rotate_view(vis):
+        nonlocal first_rotate
+        ctr = vis.get_view_control()
+        if first_rotate is False:
+            ctr.rotate(0.0, -540.0)
+            first_rotate = True
+        ctr.rotate(5.0, 0.0)
+        return False
+
+    def get_transformed_meshes(robot_sdf: pv.RobotSDF):
+        meshes = []
+        tsfs = robot_sdf.sdf.obj_frame_to_link_frame.inverse().get_matrix()
+        for i in range(len(robot_sdf.sdf_to_link_name)):
+            mesh = copy.deepcopy(robot_sdf.sdf.sdfs[i].obj_factory._mesh)
+            mesh = mesh.transform(tsfs[i].cpu().numpy())
+            meshes.append(mesh)
+        return meshes
+
     # load the panda gripper into pytorch_volumetric's RobotSDF
     urdf = os.path.join(TEST_DIR, "panda_gripper.urdf")
     chain = pk.build_chain_from_urdf(open(urdf).read())
@@ -32,42 +54,32 @@ def test_3d_contact_detection():
     residual_threshold = 3
 
     contact_detector = detection.ContactDetector(residual_precision, device=d)
-    sensor = detection.RobotResidualContactSensor(s, residual_precision, residual_threshold)
+    sensor = detection.RobotResidualContactSensor(s, residual_precision, residual_threshold, max_num_points=2000)
     # visualize the robot in open3d
     contact_detector.register_contact_sensor(sensor)
 
-    try:
-        from base_experiments.env.env import draw_ordered_end_points, aabb_to_ordered_end_points
-        from base_experiments.env.pybullet_env import DebugDrawer
-        import time
+    if visualize:
+        # visualize the robot in object frame and the cached surface points
+        obj_meshes = get_transformed_meshes(s)
 
-        # visualize the robot
-        p.connect(p.GUI)
-        p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
-        # p.setAdditionalSearchPath(search_path)
-        gripperId = p.loadURDF(urdf, [0, 0, 0], useFixedBase=True)
+        # draw the surface points using PointCloud
+        pc = o3d.geometry.PointCloud()
+        pc.points = o3d.utility.Vector3dVector(sensor._cached_points.cpu())
+        pc.paint_uniform_color([0, 1, 0])
 
-        # query_range[0, :] = query_range[0].mean()
-        # pv.draw_sdf_slice(s, query_range, resolution=0.005, device=d)
+        # also draw the surface normals using LineSet
+        length = 0.01
+        end_points = sensor._cached_points + sensor._cached_normals * length
+        pts = torch.cat([sensor._cached_points, end_points], dim=0)
+        lines = torch.arange(0, len(sensor._cached_points))
+        lines = torch.cat([lines.view(-1, 1), (lines + len(sensor._cached_points)).view(-1, 1)], dim=1)
+        normals = o3d.geometry.LineSet(
+            points=o3d.utility.Vector3dVector(pts.cpu()),
+            lines=o3d.utility.Vector2iVector(lines.cpu())
+        )
+        normals.paint_uniform_color([1, 0, 0])
 
-        vis = DebugDrawer(1., 0.5)
-        vis.toggle_3d(True)
-        vis.set_camera_position([-0.1, 0, 0], yaw=-30, pitch=-20)
-        # draw bounding box for each link (set breakpoints here to better see the link frame bounding box)
-        tfs = s.sdf.obj_frame_to_each_frame.inverse()
-        for i in range(len(s.sdf.sdfs)):
-            sdf = s.sdf.sdfs[i]
-            aabb = aabb_to_ordered_end_points(np.array(sdf.surface_bounding_box(padding=0)))
-            aabb = tfs.transform_points(torch.tensor(aabb, device=tfs.device, dtype=tfs.dtype))[i]
-            draw_ordered_end_points(vis, aabb)
-            time.sleep(0.2)
-
-        vis.draw_points("surface", sensor._cached_points, length=0.001)
-        vis.draw_2d_lines("surface.grad", sensor._cached_points, sensor._cached_normals, color=(0, 1, 0), scale=0.01)
-    except ImportError:
-        pass
-
-    input()
+        o3d.visualization.draw_geometries_with_animation_callback(obj_meshes + [pc, normals], rotate_view)
 
 
 if __name__ == "__main__":
